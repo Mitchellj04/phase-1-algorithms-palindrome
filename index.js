1c197d603df3e37975e6d8a3a76cf73859c39a05
JustinMitchell <mitchelljm04@gmail.com>
2022-02-17 22:29:15 -0500

    Completed Assignemnt

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-function isPalindrome(word) {
-  // Write your algorithm here
-}
+// const word = "bad";
+// newWord = "";
 
+// console.log(word);
+// console.log(word.length);
+
+// for (i=wordLength-1; i>= 0; i--){
+//   newWord = newWord + word.charAt(i);
+  // console.log(newWord);
+// }
+// console.log(newWord);
+
+function isPalindrome(name){
+  let newWord = ""; 
+  let wordLength = name.length;
+  for (i=wordLength-1; i>= 0; i--){
+    newWord = newWord + name.charAt(i); }
+  if (name === newWord){ return true;}
+  else {return false;}
+
+console.log(isPalindrome("ab"));
+ /*
+ Create a function that takes a string to return true if it is a palindrome. 
+ The function should be called isPalindrom. 
+ A palindrome is a word that will be the same forward and backwards 
+ example: "mom"  "madam" "dad"
+ */
 /* 
   Add your pseudocode here
+  1. initialize a function to call a string 
+  2. function should pass the string 
+  3. if the string is can be the same forward and backwards  
+  4. return true 
 */
+
 
 /*
   Add written explanation of your solution here
